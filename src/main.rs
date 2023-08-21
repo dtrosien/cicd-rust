19d66dfd5fff623793e42a8b88c3bb420f44b0e4
dtrosien <dominiktrosien@yahoo.de>
2023-08-21 17:09:39 +0200

    fix formatting

--- src/main.rs
+++ src/main.rs
@@ -1,11 +1,10 @@
 fn main() {
-    println!("{}",say_hello());
+    println!("{}", say_hello());
 }
 
-fn say_hello()-> String{
+fn say_hello() -> String {
     "Hello, world!".to_string()
 }
-
 
 #[cfg(test)]
 mod tests {
@@ -13,6 +12,6 @@ mod tests {
 
     #[test]
     fn say_hello_test() {
-        assert_eq!(say_hello(),format!("Hello, world!"))
+        assert_eq!(say_hello(), format!("Hello, world!"))
     }
 }
